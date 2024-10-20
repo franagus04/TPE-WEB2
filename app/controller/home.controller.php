56678fbfaa02c9e8583306b9af2b419d55293cf4
franagus04 <rioyo.franco@gmail.com>
2024-10-20 00:08:45 -0300

    optimizacion, refactor y comentario del codigo

--- app/controller/home.controller.php
+++ app/controller/home.controller.php
@@ -14,91 +14,46 @@
         //esta funcion establece los parametros en nulo, lo que hace que la lista se muestre completa
         public function defaultParameters(){ 
             if (!isset($_POST['vandal_rating'])) {
-                $_POST['vandal_rating'] = "null";
+                $_POST['vandal_rating'] = "NULL";
             }
             if (!isset($_POST['genre'])) {
-                $_POST['genre'] = "null";
+                $_POST['genre'] = "NULL";
             }
             if (!isset($_POST['release'])) {
-                $_POST['release'] = "null";
+                $_POST['release'] = "NULL";
             }
             if (!isset($_POST['pegi_class'])) {
-                $_POST['pegi_class'] = "null";
+                $_POST['pegi_class'] = "NULL";
             }
             if (!isset($_POST['devs'])) {
-                $_POST['devs'] = "null";
+                $_POST['devs'] = "NULL";
             }
         }
 
-        //esta funcion redacta un query basado en los filtros propuestos por el usuario
-        public function queryRedactor(){ 
-            $query = "SELECT * from listadox360";               //comienza la query con la seleccion de la tabla completa
-            
-            //agrega filtros al query segun el formulario enviado por el usuario
-            if ($_POST['vandal_rating'] != "null") {            //si el parametro no presenta el valor "null" comienza a añadir filtros al query
-                $rating_filter = $_POST['vandal_rating'];
-                if ($query == "SELECT * from listadox360") {    //si el query aun no tiene filtros comienza la concatenacion con "WHERE"
-                    if ($rating_filter == "positive") {
-                        $query .= " WHERE vandal_rating >= 8";
-                    }else if ($rating_filter == "negative"){    
-                        $query .= " WHERE vandal_rating <= 7";
-                    }
-                }else{                                          //si el query ya tiene algun filtro comienza la concatenacion con "AND"
-                    if ($rating_filter == "positive") {
-                        $query .= " AND vandal_rating >= 8";
-                    }else if ($rating_filter == "negative"){
-                        $query .= " AND vandal_rating <= 7";
-                    }
-                }                                               
-            }                                                   //el codigo repite el patron con todos los filtros aplicables
-            if ($_POST['genre'] != "null") {
-                $genre_filter = $_POST['genre'];
-                if ($query == "SELECT * from listadox360") {
-                    $query .= " WHERE genre = '".$genre_filter."'";
-                }else{
-                    $query .= " AND genre = '".$genre_filter."'";
-                }
-            }
-            if ($_POST['release'] != "null") {
-                $release_filter = $_POST['release'];
-                if ($query == "SELECT * from listadox360") {
-                    $query .= " WHERE `release` = '".$release_filter."'";
-                }else{
-                    $query .= " AND `release` = '".$release_filter."'";
-                }
-            }
-            if ($_POST['pegi_class'] != "null") {
-                $class_filter = $_POST['pegi_class'];
-                if ($query == "SELECT * from listadox360") {
-                    $query .= " WHERE pegi_class = '".$class_filter."'";
-                }else{
-                    $query .= " AND pegi_class = '".$class_filter."'";
-                }
-            }
-            if ($_POST['devs'] != "null") {
-                $devs_filter = $_POST['devs'];
-                if ($query == "SELECT * from listadox360") {
-                    $query .= " WHERE devs = '".$devs_filter."'";
-                }else{
-                    $query .= " AND devs = '".$devs_filter."'";
-                }
-            }
-            return $query;
-        }
-        public function showHome(){     //esta funcion muestra el home y le da funcionalidad
+        //esta funcion muestra el home y le da funcionalidad
+        public function showHome(){
             $this->view->showHome();    //llama a las plantillas incluidas en el view del home
-            $this->defaultParameters(); //ejecuta la funcionque establece los paramentros en nulo
+            $this->defaultParameters(); //ejecuta la funcion que establece los filtros en nulo en caso de no estar setteados
+                                   
+            //una vez cargada la pagina lee los parametros actuales de filtracion, asi sean nulos o hayan sido rellenados por el usuario
+            $rating = $_POST['vandal_rating'];
+            $genre = $_POST['genre'];
+            $release = $_POST['release'];
+            $class = $_POST['pegi_class'];
+            $devs = $_POST['devs'];
 
-            //condicional que muestra la tabla completa cuando todos los filtros estan en "null"
-            if ($_POST['vandal_rating']=="null" && $_POST['genre']=="null" && $_POST['release']=="null" && $_POST['pegi_class']=="null" && $_POST['devs']=="null") {
+            //condicional que muestra la tabla completa cuando todos los filtros estan en nulo
+            if ($_POST['vandal_rating']=="NULL" && $_POST['genre']=="NULL" && $_POST['release']=="NULL" && $_POST['pegi_class']=="NULL" && $_POST['devs']=="NULL") {
                 $hole_table = $this->model->getHoleTable();
                  $this->view->showTable($hole_table);
             }
-            //si se encuentra que un parametro no presenta el valor "null" se aplican los filtros propuestos
-            else{                           
-                $query = $this->queryRedactor();                        //redacta una query
-                $filtered_table = $this->model->applyFilter($query);    //lleva la query a una consulta
-                $this->view->showTable($filtered_table);                //muestra la tabla filtrada
+            //si se encuentra que un parametro no presenta el valor "NULL" se aplican los filtros propuestos
+            else{         
+                //redacta una query con los filtros aplicados y devuelve una tabla filtrada
+                $filtered_table = $this->model->applyFilter($rating, $genre, $release, $class, $devs);
+
+                //muestra la tabla filtrada 
+                $this->view->showTable($filtered_table);                
             }
         }
     }
--- app/model/home.model.php
+++ app/model/home.model.php
@@ -7,6 +7,7 @@
             //conexion a la base de datos
             $this->db = new PDO('mysql:host=localhost;dbname=g51_x360_db;charset=utf8', 'root', '');
         }
+
         public function getHoleTable(){
             //consulta por la tabla completa
             $query_hole_table = $this->db->prepare("SELECT * from listadox360");
@@ -17,7 +18,55 @@
 
             return $hole_table;
         }
-        public function applyFilter($query){
+
+        public function applyFilter($rating, $genre, $release, $class, $devs){ 
+            $query = "SELECT * from listadox360";               //comienza la query con la seleccion de la tabla completa
+            
+            //agrega filtros a la query segun el formulario enviado por el usuario
+            if ($_POST['vandal_rating'] != "NULL") {            //si el parametro no presenta el valor "NULL" añade el filtro a la query
+                if ($query == "SELECT * from listadox360") {    //si el query aun no tiene filtros comienza la concatenacion con "WHERE"
+                    if ($rating == "positive") {
+                        $query .= " WHERE vandal_rating >= 8";
+                    }else if ($rating == "negative"){    
+                        $query .= " WHERE vandal_rating <= 7";
+                    }
+                }else{                                          //si el query ya tiene algun filtro comienza la concatenacion con "AND"
+                    if ($rating == "positive") {
+                        $query .= " AND vandal_rating >= 8";
+                    }else if ($rating == "negative"){
+                        $query .= " AND vandal_rating <= 7";
+                    }
+                }                                               
+            }                                                   //el codigo repite el patron con todos los filtros aplicables
+            if ($_POST['genre'] != "NULL") {
+                if ($query == "SELECT * from listadox360") {
+                    $query .= " WHERE genre = '".$genre."'";
+                }else{
+                    $query .= " AND genre = '".$genre."'";
+                }
+            }
+            if ($_POST['release'] != "NULL") {
+                if ($query == "SELECT * from listadox360") {
+                    $query .= " WHERE `release` = '".$release."'";
+                }else{
+                    $query .= " AND `release` = '".$release."'";
+                }
+            }
+            if ($_POST['pegi_class'] != "NULL") {
+                if ($query == "SELECT * from listadox360") {
+                    $query .= " WHERE pegi_class = '".$class."'";
+                }else{
+                    $query .= " AND pegi_class = '".$class."'";
+                }
+            }
+            if ($_POST['devs'] != "NULL") {
+                if ($query == "SELECT * from listadox360") {
+                    $query .= " WHERE devs = '".$devs."'";
+                }else{
+                    $query .= " AND devs = '".$devs."'";
+                }
+            }
+
             //construccion de la consulta
             $query_filtered_table = $this->db->prepare($query);
     
